0b694088d17d9886e2f302ef777db24bc5c4a638
Igor Ferreira <me@igorcferreira.dev>
2023-04-24 11:42:07 +0200

    Add Metadata to ResetDefaultsStep

--- UserDefaultsPlugin/UserDefaultsPlugin/ResetDefaultsStep.swift
+++ UserDefaultsPlugin/UserDefaultsPlugin/ResetDefaultsStep.swift
@@ -8,17 +8,16 @@ import Foundation
 import MobileWorkflowCore
 import SwiftUI
 
-public class ResetDefaultsStep: ObservableStep {
+public class ResetDefaultsStep: ObservableStep, BuildableStepWithMetadata {
+    public let properties: UserDefaultsResetDefaultsMetadata
 
+    public required init(properties: UserDefaultsResetDefaultsMetadata, session: Session, services: StepServices) {
+        self.properties = properties
+        super.init(identifier: properties.id, session: session, services: services)
+    }
+    
     public override func instantiateViewController() -> StepViewController {
         ResetDefaultsStepViewController(step: self)
-    }
-}
-
-extension ResetDefaultsStep: BuildableStep {
-
-    public static func build(stepInfo: StepInfo, services: StepServices) throws -> Step {
-        return ResetDefaultsStep(identifier: stepInfo.data.identifier, session: stepInfo.session, services: services)
     }
 }
 
@@ -46,4 +45,27 @@ public class ResetDefaultsStepViewController: MWLoadingStepViewController {
     
 }
 
+public class UserDefaultsResetDefaultsMetadata: StepMetadata {
+    init(id: String, title: String, next: PushLinkMetadata?, links: [LinkMetadata]) {
+        super.init(id: id, type: "io.app-rail.userdefaults.reset-defaults", title: title, next: next, links: links)
+    }
+    
+    public required init(from decoder: Decoder) throws {
+        try super.init(from: decoder)
+    }
+    
+    public override func encode(to encoder: Encoder) throws {
+        try super.encode(to: encoder)
+    }
+}
 
+public extension StepMetadata {
+    static func userDefaultsResetDefaults(id: String, title: String, next: PushLinkMetadata? = nil, links: [LinkMetadata] = []) -> UserDefaultsResetDefaultsMetadata {
+        UserDefaultsResetDefaultsMetadata(
+            id: id,
+            title: title,
+            next: next,
+            links: links
+        )
+    }
+}
